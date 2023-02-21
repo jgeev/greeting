933a715ded11b13692faf3813a345a72f1bf3ee0
jgeev <jincy.varghese.ann@gmail.com>
2023-02-20 19:58:19 -0600

    updated the test

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
-var utils  = require('course-utilities');
-var hello = utils.load('./hello.js', 'hello');
+var utils = require("course-utilities");
+var hello = utils.load("./hello.js", "hello");
 
-test('outputs the correct string', () => {
-  expect(hello()).toBe("Hello World!");
+test("outputs the correct string", () => {
+  expect(hello()).toBe("Hello World");
 });
